aa5e1f2d86f92f27029c2361b8f9632f199c4b4c
vladislav fomin <thefomas.1@gmail.com>
2021-03-08 12:01:51 +0300

    Исправлена реализация Service

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -12,72 +14,29 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+    UserDao ud = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        try (Statement statement = Util.connectDB().createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS users " +
-                    "(id BIGINT primary key not null auto_increment," +
-                    "name VARCHAR(50) not null," +
-                    "lastName VARCHAR(50)," +
-                    "age TINYINT )");
-        } catch (SQLException e) {
-            System.out.println("Create table Exception");
-        }
+        ud.createUsersTable();
     }
 
     public void dropUsersTable() {
-        try (Statement statement = Util.connectDB().createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS users");
-        } catch (SQLException e) {
-            System.out.println("Дроптэйбл экзепшн");
-        }
+        ud.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Statement statement = Util.connectDB().createStatement()) {
-            statement.executeUpdate(String.format("INSERT INTO users (name, lastName, age) " +
-                            "VALUES(%s, %s, %d)", name, lastName, age));
-            System.out.printf("User named %s added successfully\n", name);
-        } catch (SQLException e) {
-            System.out.println("Добавление юзера экзепшшн");
-            e.printStackTrace();
-        }
+        ud.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        try (Statement statement = Util.connectDB().createStatement()) {
-            statement.executeUpdate(String.format("DELETE FROM users WHERE id = %d", id));
-        } catch (SQLException e) {
-            System.out.println("Удаление юзера экзепшн");
-        }
+        ud.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        List<User> list = new ArrayList<>();
-        try (Statement statement = Util.connectDB().createStatement()) {
-           ResultSet rs = statement.executeQuery("SELECT * FROM usersdb.users");
-           while (rs.next()) {
-               // какой ещё есть способ?
-               String name = rs.getString("name");
-               String lastName = rs.getString("lastName");
-               byte age = rs.getByte("age");
-
-               User user = new User('\'' + name + '\'', '\'' + lastName + '\'', age);
-               System.out.println(user.toString());
-               list.add(user);
-           }
-           return list;
-        } catch (SQLException e) {
-            System.out.println("Юзерсгет экзепшн");
-        }
-        return null;
+        return ud.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = Util.connectDB().createStatement()) {
-            statement.executeUpdate("TRUNCATE TABLE users");
-        } catch (SQLException e) {
-            System.out.println("Транкейт экзепшн");
-        }
+        ud.cleanUsersTable();
     }
 }
